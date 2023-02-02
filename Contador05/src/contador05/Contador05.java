6719f5b6e728c1f43aba91b57d143414d487709b
Adrian Faria de Souza <116755380+AdriannSouza@users.noreply.github.com>
2023-02-02 10:39:24 -0300

    Estrutura de repetição 'for'

---
+++ Contador05/src/contador05/Contador05.java
@@ -0,0 +1,11 @@
+package contador05;
+
+public class Contador05 {
+
+    public static void main(String[] args) {
+        for (int cc = 1; cc <= 4; cc++) {
+            System.out.println("Cambalhota " + cc);
+        }
+    }
+    
+}
